088b94a8e803ee9971e32e7fc708199cefd5b7bd
Hakjun Kim <hakie2kim@gmail.com>
2024-08-13 14:55:44 +0900

    etc: swagger로 API 문서 작성

---
+++ src/main/java/kr/co/ordermanagement/configuration/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package kr.co.ordermanagement.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Order Management Application")
+                .description("상품을 주문할 수 있는 주문 관리에 관한 REST API");
+    }
+
+}
